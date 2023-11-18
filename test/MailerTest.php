b265f3a55791882b1820a7a6501803246b5bede9
Daniel Suniaga <danieldsuniaga@gmail.com>
2023-11-18 14:14:54 -0500

    first commit

---
+++ test/MailerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'src/Mailer.php';
+
+use PHPUnit\Framework\TestCase;
+
+class MailerTest extends TestCase
+{
+    public function testSendMessageReturnsTrue()
+    {
+        $this->assertTrue(Mailer::send('dave@example.com', 'Hello!'));
+    }
+    
+    public function testInvalidArgumentExceptionIfEmailIsEmpty()
+    {
+        $this->expectException(InvalidArgumentException::class);
+            
+        Mailer::send('', '');
+    }      
+}
